b1d978cf13f56e9752988d1a574c0992cb965899
mahima <mahima@vishwadeep>
2018-07-01 20:13:36 +0530

    added a for loop

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc src/program1.c -o bin/program1
--- src/program1.c
+++ src/program1.c
@@ -3,5 +3,10 @@
 int main ()
 {
 	printf("Hello world\n");
+	int i = 0;
+	for (i = 0; i < 5; i++)
+	{
+		printf("here is the loop for = %d\n", i);
+	}
 	return 0;
 }
